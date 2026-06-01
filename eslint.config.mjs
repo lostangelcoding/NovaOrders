a647afa43574dbc8eb1ad9531ec52231c52ec2f9
Ivan M. <angelcodestudio@gmail.com>
2026-06-01 19:28:51 +0200

    Add ESLint config and dependencies

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+export default [
+  {
+    languageOptions: { 
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      } 
+    }
+  },
+  pluginJs.configs.recommended,
+  {
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "off"
+    }
+  }
+];
