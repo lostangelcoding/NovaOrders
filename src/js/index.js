ecba486596cc4e21ad97b732a9501ac32cfb85ea
Ivan M. <angelcodestudio@gmail.com>
2026-06-04 14:29:59 +0200

    Add new imports & refactor call functions

--- src/js/index.js
+++ src/js/index.js
@@ -1,38 +1,57 @@
 // Imports 
-import { fetchServices, getAllServices, renderServices } from "/src/js/modules/services.js";
+import { handleSearchFromURL } from "/src/js/modules/search.js";
+import { initServiceFilters } from "/src/js/modules/serviceFilter.js";
+import { initDropdownFilter } from "/src/js/modules/dropdown.js";
+import { fetchServices, renderServices } from "/src/js/modules/services.js";
 import { loadPartial } from "/src/js/core/loadPartials.js";
 
-// Setup
 const page = document.body.dataset.page;
 
-addEventListener('DOMContentLoaded', async () => {
-    // Partials
+// Functions
+function handleServicesRendering(page, allServices) {
+    if (page === 'services') {
+        renderServices('services-list', allServices);
+    } else if (page === 'home') {
+        const popularOnly = allServices.filter(service => service.popular);
+        renderServices('services-preview', popularOnly);
+    }
+}
+
+async function initPartials(page) {
     await loadPartial('header', '/src/partials/header.html');
     await loadPartial('footer', '/src/partials/footer.html');
     
     if(page === "services") {
         await loadPartial('header-services', '/src/partials/header-services.html');
-
-    } else if(page === "service") {
-        await loadPartial('', '');
-        
-    } else if(page === "contact") {
-        await loadPartial('', '');
     }
+}
 
-    // Services
+async function initServicesModule(page) {
+    if(page !== 'home' && page !== 'services') return;
+    
     try {
         const allServices = await fetchServices();
 
-        if(page === 'services') {
-            renderServices('services-list', allServices);
-        } else if (page === 'home') {
-            const popularOnly = allServices.filter(services => services.popular);
-            renderServices('services-preview', popularOnly);
-        }
+        handleServicesRendering(page, allServices);
 
+        if (page === 'services') {
+            initServiceFilters(allServices, 'services-list');
+            handleSearchFromURL('services-list');
+        }
     } catch(error) {
         console.error("Critical Error: Failed to initialize the services module:", error);
     }
+}
+
+// DOM Setup 
+addEventListener('DOMContentLoaded', async () => {
+    // Partials
+    await initPartials(page);
+
+    // Services
+    await initServicesModule(page);
+
+    // Dropdown
+    initDropdownFilter();
 });
 
