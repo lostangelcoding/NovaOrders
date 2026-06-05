a582df7efed98aacd50a5d0b554771a59e6486fb
Ivan M. <angelcodestudio@gmail.com>
2026-06-05 15:55:31 +0200

    Add styles & logic for toast

---
+++ src/js/utils/toast.js
@@ -0,0 +1,11 @@
+export function showToast(message, type = 'success') {
+    const toast = document.getElementById('toast');
+    if (!toast) return;
+
+    toast.textContent = message;
+    toast.className = `toast show ${type}`;
+
+    setTimeout(() => {
+        toast.classList.remove('show');
+    }, 3000);
+}
