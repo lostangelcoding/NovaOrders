ab2b1619224a1f6563d10240d03d24647d66ab6c
Ivan M. <angelcodestudio@gmail.com>
2026-06-06 23:22:16 +0200

    Fix missing payload in email service fetch call

--- src/js/modules/email.js
+++ src/js/modules/email.js
@@ -2,10 +2,10 @@ const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbx9SfqA_NLIS0h5jAX4-
 
 export async function sendEmail(formData) {
   try {
-    // Zmieniamy na zwykły fetch z poprawną obsługą CORS
     const response = await fetch(SCRIPT_URL, {
       method: 'POST',
       mode: 'cors', 
+      body: formData 
     });
 
     if (response.ok) {
